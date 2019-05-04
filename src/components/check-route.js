e8b31470d9bf57677a7629adc7bb62bc06a67bb1
Jason D'souza <jvincentdsouza@gmail.com>
2019-05-04 21:07:56 +1000

    added datetime formatting for current time and date

--- src/components/check-route.js
+++ src/components/check-route.js
@@ -1,28 +1,36 @@
 import React, {Component} from 'react';
-import {check, test} from '../utils/availability-check';
+import {useStaticQuery, graphql} from 'gatsby';
 import {MEETUP_GREET, MEETUP_NAME, MEETUP_URL} from "../../configure";
 
 //react components
 import SignUp from './main-page';
+import {currentDateTime,
+        timeCheck,
+        dateCheck,
+        locationCheck} from '../utils/availability-check';
 
-export default class Routing extends Component {
-    constructor() {
-        super();
-        this.state = {
-            route: 'home'
-        }
-    }
-    
-    
+// This component is designed to check the time, date and location of the user and whether they're
+// in the correct location at the right time to allow them to sign up to the meetup's slack channel
 
-    render() {
-        return (
-            <div>
-                {this.state.route === 'home' ? 
-                    <div>{check()}<SignUp meetupGreet={MEETUP_GREET}/></div> 
-                    : 
-                    <div>Apology</div>}
-            </div>
-        )
-    }
+export default () => {
+    //query the meetup source plugin for date and time
+    const data = useStaticQuery(
+        graphql`
+            query {
+                meetupEvent {
+                    local_time
+                    local_date
+                }
+            }
+        
+        `
+    )
+
+    const now = currentDateTime().currentTime + '-' + currentDateTime().currentDate;
+    
+    return (
+        <div>
+            <div><SignUp meetupGreet={MEETUP_GREET}/>{now} - {data.meetupEvent.local_time}</div> 
+        </div>
+    )
 }
--- src/utils/availability-check.js
+++ src/utils/availability-check.js
@@ -9,21 +9,46 @@ import {graphql, useStaticQuery} from 'gatsby';
 //   starting time of the event
 // - requires the same for date
 // - must also terminate after duration is over
+// - possibly 
 //route to index
 //otherwise route to apology pages
 
 //Time and location
 
-const check = () => {
-    return 'hello'
+const currentDateTime = () => {
+
+    //obtain data to get dates
+    const datetime = new Date();
+    //get and format date into yyyymmdd
+    let date = String(datetime.getFullYear()*10000 + (datetime.getMonth()+1)*100)
+    const day = datetime.getDate()
+    if (day < 10){
+        date += '0' + String(day) //to keep the formatting at yyyymmdd and not yyyymmd
+    } else {
+        date += day
+    }
+    const today = date; //store as a constant
+    //get and format hours and minutes in 24 hours time (hhmm)
+    const time = datetime.getHours() * 100 + datetime.getMinutes()
+
+    return {currentTime: time, currentDate: today};
 }
 
-const test = () => {
-    return `hi`
-    
+const timeCheck = (eventTime) => {
+    //checks the current time against the event time, in 24 hours formatting
+}
+
+const dateCheck = (eventDate) => {
+    //checks the current date against the event date in yyyymmdd formatting
+}
+
+const locationCheck = (option) => {
+    //checks the current location against the location of the user by either IP or lat/long coordinates
 }
 
 export {
-    check,
-    test
+    currentDateTime,
+    timeCheck,
+    dateCheck,
+    locationCheck
 }
